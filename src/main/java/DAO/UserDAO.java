fc3d40470a9debbb464c8591253300df1dac7f88
omarabdelaz1z <omarabdelaziz042@gmail.com>
2020-12-30 06:26:38 +0200

    Refactor: delete studentDAO and staffMemberDAO

---
+++ src/main/java/DAO/UserDAO.java
@@ -0,0 +1,29 @@
+package DAO;
+
+import Database.HibernateUtil;
+import Entities.CRUD.ICRUD;
+import Entities.CRUD.UserCRUD;
+import Entities.User;
+
+import java.util.List;
+
+public class UserDAO {
+    private final ICRUD<User> userCRUD;
+
+    public UserDAO() {
+        userCRUD = new UserCRUD(HibernateUtil.getInstance());
+    }
+
+    public User validate(String email, String password) {
+        List<User> users = userCRUD.query("from User O where O.email = '" + email + "' AND O.password = '" + password + "'");
+        try {
+            return users.get(0);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public User create(User object){
+        return userCRUD.create(object);
+    }
+}
